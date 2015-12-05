730fd9045e34ec7fc618d41a2d8351a78f95d856
Henri <t.henri.al@gmail.com>
2015-12-05 17:40:16 +0100

    Site

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # SGBD
+Install mysql :
+===============
 
-sudo apt-get install pip
-pip install FLask
+sudo apt-get install mysql-server
+
+
+
+Install python :
+=================
+
+sudo apt-get install python-dev
+
+
+Setup Flask and mysql-python
+============================
+
+sudo apt-get install python-pip
+pip install mysql-python
+pip install Flask
+python ez_setup.py
 pip install flask-mysql
 chmod 700 run.py
-./run.py
+
+Setup mysql
+===========
+mysql -u root -p
+CREATE DATABASE ProjetSGBD
+\. creation_base
+\. donnees_etendues
--- run.py
+++ run.py
@@ -7,34 +7,38 @@ mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
-app.config['MYSQL_DATABASE_DB'] = 'EmpData'
+app.config['MYSQL_DATABASE_DB'] = 'ProjetSGBD'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 @app.route("/")
 @app.route("/index")
-def index():
-	user = {'nickname': 'JeanPierre'}
-	posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        }
-    ]
-	return render_template('index.html',title='Projet SGBD', user=user, posts = posts)
-
-
-@app.route("/test")
-def test():
-	user = {'nickname': 'JeanPierre'}
+@app.route("/index/<categorie>")
+def index(categorie=None):
 	cursor = mysql.connect().cursor()
-	query = "SELECT * FROM User"
+	if (categorie == None):
+		query = "SELECT Recette.Id_recette, Nom_recette, Nb_personnes, Budget, COUNT(Date_avis), (avg( Note_qualite ) + avg( Note_justesse ) + avg( Note_respect )) /3 AS Note, Url_image FROM Recette LEFT OUTER JOIN Avis ON Recette.Id_recette = Avis.Id_recette GROUP BY Id_recette, Nom_recette, Nb_personnes, Budget, Url_image"
+	else:
+		query = "SELECT Recette.Id_recette, Nom_recette, Nb_personnes, Budget, COUNT(Date_avis), (avg( Note_qualite ) + avg( Note_justesse ) + avg( Note_respect )) /3 AS Note, Url_image FROM Recette LEFT OUTER JOIN Avis ON Recette.Id_recette = Avis.Id_recette WHERE Recette.Categorie_recette = '" + categorie + "' GROUP BY Id_recette, Nom_recette, Nb_personnes, Budget, Url_image"
 	cursor.execute(query)
-	data = cursor.fetchall()
-	return render_template('test.html',title='Projet SGBD', user=user, data=data)
+	recettes = cursor.fetchall()
+
+	return render_template('index.html', recettes=recettes, categorie=categorie)
+
+@app.route("/recette/<Id_recette>")
+def recettes(Id_recette):
+	cursor = mysql.connect().cursor()
+	query = "SELECT * FROM Recette WHERE Recette.Id_recette = '" + Id_recette + "'"
+	cursor.execute(query)
+	recette = cursor.fetchone()
+
+	query = "SELECT Commentaire, Date_commentaire, Nom_eleve FROM Recette NATURAL JOIN Commenter NATURAL JOIN Eleve WHERE Id_recette = '" + Id_recette + "'"
+	cursor.execute(query)
+	commentaires = cursor.fetchall()
+
+	query = "SELECT * FROM Recette NATURAL JOIN Avis WHERE Id_recette = '" + Id_recette + "'"
+	cursor.execute(query)
+	avis = cursor.fetchall()
+	return render_template('recette.html', recette=recette, commentaires=commentaires, avis=avis)
 
 app.run(debug=True)
