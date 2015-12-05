800884bbe288bfa0949ed81bab94e9580e779e0a
GANGLOFF Nolwenn <nolwenn.gangloff@gmail.com>
2015-12-05 10:38:06 +0100

    tg

--- requetes.sql
+++ requetes.sql
@@ -1,3 +1,14 @@
+-- OK suppression Eleve -> recette associée id_eleve = null 
+--
+--
+--
+--
+--
+--
+--
+--
+
+
 -- insertion
 insert into Eleve (Nom_eleve,Prenom_eleve) values ("Gangloff", "Nolwenn");
 insert into Eleve (Nom_eleve,Prenom_eleve,Date_inscription) values ("Gruchet", "Seb", "1994-03-13");
@@ -5,6 +16,15 @@ insert into Eleve (Nom_eleve,Prenom_eleve,Date_inscription) values ("Gruchet", "
 
 insert into Recette (Nom_recette,Budget,Difficulte,Temps_preparation,Temps_cuisson,Etapes,Categorie_recette,Id_eleve)
 values ("Tiramisu", 30, 2,15,0,"battez les oeufs, mettez le mascarpone","Dessert",1);
+
+
+
+ALTER TABLE Recette DROP FOREIGN KEY fk_parent;
+ALTER TABLE Recette ADD CONSTRAINT fk_parent
+	FOREIGN KEY (id_parent)
+	REFERENCES parent(id)
+	ON DELETE SET NULL
+	ON UPDATE SET NULL;
 
 
 insert into Commenter (Id_eleve,Id_recette, Commentaire, Date_commentaire) values (1,1,"C'est de la balle !","2014-01-01");
@@ -17,6 +37,10 @@ natural join Commenter,
 natural join Recette
 group by Eleve.Nom_eleve;
 
+
+SET FOREIGN_KEY_CHECKS=0;
+delete from Eleve;
+SET FOREIGN_KEY_CHECKS=1;
 
 -- moyenne des prix des recettes proposées par un élève donné
 
